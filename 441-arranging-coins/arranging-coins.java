831749b3dda1773fb0fc41d2dd97650ffc71369d
Devansh Jain <jdevansh360@gmail.com>
2026-08-01 00:31:36 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (81.87%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,16 +1,18 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int x=n;
-        int y=0;
-        if(n==0) return 0;
-        for(int i=1;i<=n;i++){
-            if(x>=i){
-            x=x-i;
-            y=i;
-            }
-            else break;
-        }
-        if(n>=0) return y;
-        return 0;
+        // int x=n;
+        // int y=0;
+        // if(n==0) return 0;
+        // for(int i=1;i<=n;i++){
+        //     if(x>=i){
+        //     x=x-i;
+        //     y=i;
+        //     }
+        //     else break;
+        // }
+        // if(n>=0) return y;
+        // return 0;
+        long m = (long) n;
+        return (int) ((Math.sqrt(8 * m + 1) - 1) / 2);
     }
 }
